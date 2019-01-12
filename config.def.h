0f1d7cc58d60c4f7b1b036792381c1f0f7a90b06
Filip Kokosiński <filip.kokosinski@gmail.com>
2019-01-12 21:35:24 +0100

    Change bindings for searching and opening URLs

--- config.def.h
+++ config.def.h
@@ -216,8 +216,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_I,           iso14755,       {.i =  0} },
 	{ XK_NO_MOD,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ XK_NO_MOD,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ TERMMOD,              XK_N,           opencopied,     {.v = "firefox"} },
-	{ TERMMOD,              XK_M,           copyurl,        {.i =  0} },
+	{ TERMMOD,              XK_W,           opencopied,     {.v = "xdg-open"} },
+	{ TERMMOD,              XK_Q,           copyurl,        {.i =  0} },
 };
 
 /*
